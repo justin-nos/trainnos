c5e29f09e4472f5ec987b3c39a986578cb21eac7
justin-nos <justin@trainnos.com>
2024-12-25 11:57:50 -0600

    Added PostHog analytics
    Added NOS favicon

--- app/layout.tsx
+++ app/layout.tsx
@@ -4,6 +4,7 @@ import Navbar from "../components/client/navbar";
 import Footer from "../components/server/footer";
 import {SpeedInsights} from "@vercel/speed-insights/next";
 import {Analytics} from "@vercel/analytics/react";
+import {CSPostHogProvider} from "./providers";
 const inter = Inter({subsets: ["latin"]});
 
 export const metadata = {
@@ -15,11 +16,13 @@ export default function RootLayout({children}: {children: React.ReactNode}) {
   return (
     <html lang="en">
       <body className={inter.className}>
-        <Navbar />
-        {children}
-        <SpeedInsights />
-        <Analytics />
-        <Footer />
+        <CSPostHogProvider>
+          <Navbar />
+          {children}
+          <SpeedInsights />
+          <Analytics />
+          <Footer />
+        </CSPostHogProvider>
       </body>
     </html>
   );
