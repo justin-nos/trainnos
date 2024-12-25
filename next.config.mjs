c0bbd83abcad0832476001a473c8322b4ef25101
justin-nos <justin@trainnos.com>
2024-12-25 12:40:05 -0600

    Adding reverse proxy for posthog

--- next.config.mjs
+++ next.config.mjs
@@ -1,30 +1,13 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  exports: {
-    images: {
-      remotePatterns: [
-        {
-          protocol: "https",
-          hostname: "images.unsplash.com",
-          pathname: "/photo-1545972154-9bb223aac798",
-          search:
-            "?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=3050&q=80&exp=8&con=-15&sat=-75",
-        },
-      ],
-    },
-  },
-};
-const defaultExp = {
-  images: {
-    remotePatterns: [
+  reactStrictMode: true,
+  async rewrites() {
+    return [
       {
-        protocol: "https",
-        hostname: "images.unsplash.com",
-        pathname: "/photo-1545972154-9bb223aac798",
-        search:
-          "?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=3050&q=80&exp=8&con=-15&sat=-75",
+        source: "/ingest/:path*",
+        destination: "https://app.posthog.com/:path*",
       },
-    ],
+    ];
   },
 };
-export default defaultExp;
+module.exports = nextConfig;
