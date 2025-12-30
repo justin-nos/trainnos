5fc38df5d6d2feeceae3425d9e395453ae65d242
justin-nos <justin@trainnos.com>
2025-12-30 16:48:30 -0600

    please

--- components/client/CheckForAuth.tsx
+++ components/client/CheckForAuth.tsx
@@ -1,6 +1,5 @@
 "use client";
 import {useSession} from "next-auth/react";
-import {useRouter} from "next/navigation";
 import {signIn} from "next-auth/react";
 import {useEffect, useState} from "react";
 
